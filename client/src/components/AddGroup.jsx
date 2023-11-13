57a1eb75e574a8d5fc0529495b4bd9a2d4920b0b
Boris Boev <boris.boev@gmail.com>
2023-11-13 21:14:20 +0200

    More code :)

--- client/src/components/AddGroup.jsx
+++ client/src/components/AddGroup.jsx
@@ -4,7 +4,7 @@ import styles from "./AddGroup.module.css";
 export default function AddGroup() {
   return (
     <div className={styles.addForm}>
-      <h3>Add Group</h3>
+      <h3>Add New Cruise Group</h3>
       <form method="post" action="#">
         <div className="row uniform">
           <div className="6u 12u$(xsmall)">
@@ -46,7 +46,45 @@ export default function AddGroup() {
             />
           </div>
           <div className="6u 12u$(xsmall)">
-            <label htmlFor="duration">Duration</label>
+            <label htmlFor="from">Departure:</label>
+            <input
+              type="date"
+              name="from"
+              id="from"
+              defaultValue
+              placeholder="from"
+            />
+            <div
+              data-lastpass-icon-root="true"
+              style={{
+                position: "relative !important",
+                height: "0px !important",
+                width: "0px !important",
+                float: "left !important",
+              }}
+            />
+          </div>
+          <div className="6u 12u$(xsmall)">
+            <label htmlFor="to">Return:</label>
+            <input
+              type="date"
+              name="to"
+              id="to"
+              defaultValue
+              placeholder="to"
+            />
+            <div
+              data-lastpass-icon-root="true"
+              style={{
+                position: "relative !important",
+                height: "0px !important",
+                width: "0px !important",
+                float: "left !important",
+              }}
+            />
+          </div>
+          <div className="2u 12u$(xsmall)">
+            <label htmlFor="duration">Duration:</label>
             <input
               type="number"
               name="duration"
@@ -64,7 +102,7 @@ export default function AddGroup() {
               }}
             />
           </div>
-          <div className="6u 12u$(xsmall)">
+          <div className="10u 12u$(xsmall)">
             <label htmlFor="itinerary">ImageUrl</label>
             <input
               type="text"
@@ -77,53 +115,16 @@ export default function AddGroup() {
           <div className="12u$">
             <div className="select-wrapper">
               <select name="category" id="category">
-                <option value>- Category -</option>
-                <option value={1}>Manufacturing</option>
-                <option value={1}>Shipping</option>
-                <option value={1}>Administration</option>
-                <option value={1}>Human Resources</option>
+                <option value>- Transport -</option>
+                <option value={1}>Flight</option>
+                <option value={1}>Bus</option>
+                <option value={1}>Car</option>
+              
               </select>
             </div>
           </div>
           {/* Break */}
-          <div className="4u 12u$(small)">
-            <input
-              type="radio"
-              id="priority-low"
-              name="priority"
-              defaultChecked
-            />
-            <label htmlFor="priority-low">Low Priority</label>
-          </div>
-          <div className="4u 12u$(small)">
-            <input type="radio" id="priority-normal" name="priority" />
-            <label htmlFor="priority-normal">Normal Priority</label>
-          </div>
-          <div className="4u$ 12u$(small)">
-            <input type="radio" id="priority-high" name="priority" />
-            <label htmlFor="priority-high">High Priority</label>
-          </div>
-          {/* Break */}
-          <div className="6u 12u$(small)">
-            <input type="checkbox" id="copy" name="copy" />
-            <label htmlFor="copy">Email me a copy of this message</label>
-          </div>
-          <div className="6u$ 12u$(small)">
-            <input type="checkbox" id="human" name="human" defaultChecked />
-            <label htmlFor="human">I am a human and not a robot</label>
-          </div>
-          {/* Break */}
-          <div className="12u$">
-            <textarea
-              name="message"
-              id="message"
-              placeholder="Enter your message"
-              rows={6}
-              data-gramm="false"
-              wt-ignore-input="true"
-              defaultValue={""}
-            />
-          </div>
+        
           {/* Break */}
           <div className="12u$">
             <ul className="actions">
@@ -137,99 +138,7 @@ export default function AddGroup() {
           </div>
         </div>
       </form>
-      <form method="post" action="#">
-        <div className="row uniform">
-          <div className="6u 12u$(xsmall)">
-            <input
-              type="text"
-              name="name"
-              id="name"
-              defaultValue
-              placeholder="Name"
-            />
-            <div
-              data-lastpass-icon-root="true"
-              style={{
-                position: "relative !important",
-                height: "0px !important",
-                width: "0px !important",
-                float: "left !important",
-              }}
-            />
-          </div>
-          <div className="6u$ 12u$(xsmall)">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              defaultValue
-              placeholder="Email"
-            />
-          </div>
-          {/* Break */}
-          <div className="12u$">
-            <div className="select-wrapper">
-              <select name="category" id="category">
-                <option value>- Category -</option>
-                <option value={1}>Manufacturing</option>
-                <option value={1}>Shipping</option>
-                <option value={1}>Administration</option>
-                <option value={1}>Human Resources</option>
-              </select>
-            </div>
-          </div>
-          {/* Break */}
-          <div className="4u 12u$(small)">
-            <input
-              type="radio"
-              id="priority-low"
-              name="priority"
-              defaultChecked
-            />
-            <label htmlFor="priority-low">Low Priority</label>
-          </div>
-          <div className="4u 12u$(small)">
-            <input type="radio" id="priority-normal" name="priority" />
-            <label htmlFor="priority-normal">Normal Priority</label>
-          </div>
-          <div className="4u$ 12u$(small)">
-            <input type="radio" id="priority-high" name="priority" />
-            <label htmlFor="priority-high">High Priority</label>
-          </div>
-          {/* Break */}
-          <div className="6u 12u$(small)">
-            <input type="checkbox" id="copy" name="copy" />
-            <label htmlFor="copy">Email me a copy of this message</label>
-          </div>
-          <div className="6u$ 12u$(small)">
-            <input type="checkbox" id="human" name="human" defaultChecked />
-            <label htmlFor="human">I am a human and not a robot</label>
-          </div>
-          {/* Break */}
-          <div className="12u$">
-            <textarea
-              name="message"
-              id="message"
-              placeholder="Enter your message"
-              rows={6}
-              data-gramm="false"
-              wt-ignore-input="true"
-              defaultValue={""}
-            />
-          </div>
-          {/* Break */}
-          <div className="12u$">
-            <ul className="actions">
-              <li>
-                <input type="submit" defaultValue="Send Message" />
-              </li>
-              <li>
-                <input type="reset" defaultValue="Reset" className="alt" />
-              </li>
-            </ul>
-          </div>
-        </div>
-      </form>
+     
     </div>
   );
 }
