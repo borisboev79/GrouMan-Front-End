339c5021c818836a25fa59c83a4a35c494afb0b6
Boris Boev <boris.boev@gmail.com>
2023-11-14 23:17:15 +0200

    formaaaat

--- client/src/components/GroupCard.jsx
+++ client/src/components/GroupCard.jsx
@@ -13,24 +13,38 @@ export default function GroupCard({
   price,
 }) {
   return (
-     <div className={[styles.flexItem, styles.box].join(" ")}>
+    <div className={[styles.flexItem, styles.box].join(" ")}>
       <div className={[styles.image, styles.fit].join(" ")}>
-        <img src={imageUrl} alt={_id} width={418} height={200}  />
+        <img src={imageUrl} alt={_id} width={418} height={200} />
       </div>
-      <div className={styles.content}>
+      <div>
         <h3>{name}</h3>
-        <div>
-        <p><b>Transportation: </b>{transportation}</p>
-        <p><b>Itinerary: </b>{itinerary}</p>
-        <p><b>Duration: {duration}</b> дни</p>
+        <div className={styles.cardContent}>
+          <p>
+            <b>Transportation: </b>
+            {transportation}
+          </p>
+          <p>
+            <b>Itinerary: </b>
+            {itinerary}
+          </p>
+          <p>
+            <b>Duration: {duration}</b> дни
+          </p>
         </div>
-        <p><b>Начална дата: </b>{dateFormatter.formatDate(from)}</p>
-        <p><b>Крайна дата: </b> {dateFormatter.formatDate(to)}</p>
-        <p className={styles.price}>от<b className={styles.priceTag}> €{price}</b></p>  
-        <button className={[styles.fit, styles.small].join(" ")}>
-          Details
-        </button>
+        <p>
+          <b>Начална дата: </b>
+          {dateFormatter.formatDate(from)}
+        </p>
+        <p>
+          <b>Крайна дата: </b> {dateFormatter.formatDate(to)}
+        </p>
+        <p className={styles.price}>
+          от<b className={styles.priceTag}> €{price}</b>
+        </p>
+           <button className={[styles.fit, styles.small].join(" ")}>Details</button>
       </div>
+   
     </div>
   );
 }
