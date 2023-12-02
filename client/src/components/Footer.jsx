a704932b4c09a2437e0aeb5ef0550ec01c108a69
Boris Boev <boris.boev@gmail.com>
2023-12-02 11:11:35 +0200

    fixed footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,29 +1,27 @@
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
 
 export default function Footer() {
-    return(
-        <>
-        <footer className={styles.footer}>
-        <div className={styles.inner}>
-          <h3>For Assistance</h3>
-          <ul className={styles.actions}>
-            <li>
-              <span className="icon fa-phone" /> <a href="#">(+359) 888-9876543</a>
-            </li>
-            <li>
-              <span className="icon fa-envelope" />{" "}
-              <a href="#">boris.boev@grouman.bg</a>
-            </li>
-            <li>
-              <span className="icon fa-map-marker" /> Sofia 1164, Bulgaria 2000 Building
-        
-            </li>
-          </ul>
-        </div>
+  return (
+    <>
+      <footer className={styles.footer}>
+        <ul className={styles.actions}>
+          <li>
+            <span className="icon fa-phone" />{" "}
+            <a href="#">(+359) 888-9876543</a>
+          </li>
+          <li>
+            <span className="icon fa-envelope" />{" "}
+            <a href="#">boris.boev@grouman.bg</a>
+          </li>
+          <li>
+            <span className="icon fa-map-marker" /> Sofia 1164, Bulgaria 2000
+            Building
+          </li>
+        </ul>
+        <p className={styles.copy}>
+          Design by: Boris Boev & TEMPLATED November 2023 &copy;
+        </p>
       </footer>
-      <div className={styles.copyright}>
-        <p>Design by: Boris Boev & TEMPLATED November 2023 &copy;</p>
-      </div>
-      </>
-    )
+    </>
+  );
 }
