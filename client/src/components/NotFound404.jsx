4bed52d84b6b6aca6e7e7cda7aa7071a252054ee
Boris Boev <boris.boev@gmail.com>
2023-12-02 03:21:14 +0200

    fixed 404 page

--- client/src/components/NotFound404.jsx
+++ client/src/components/NotFound404.jsx
@@ -1,14 +1,10 @@
+import './NotFound404.css';
+
 export default function NotFound404(){
     return(
-        <>
-        <h1>No Such Page. This is the meaning of 404</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit nostrum esse labore expedita ipsum sed explicabo dignissimos animi hic. Perspiciatis voluptas iste illo! Sapiente quidem numquam expedita, maiores modi quaerat?</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit nostrum esse labore expedita ipsum sed explicabo dignissimos animi hic. Perspiciatis voluptas iste illo! Sapiente quidem numquam expedita, maiores modi quaerat?</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit nostrum esse labore expedita ipsum sed explicabo dignissimos animi hic. Perspiciatis voluptas iste illo! Sapiente quidem numquam expedita, maiores modi quaerat?</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit nostrum esse labore expedita ipsum sed explicabo dignissimos animi hic. Perspiciatis voluptas iste illo! Sapiente quidem numquam expedita, maiores modi quaerat?</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit nostrum esse labore expedita ipsum sed explicabo dignissimos animi hic. Perspiciatis voluptas iste illo! Sapiente quidem numquam expedita, maiores modi quaerat?</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit nostrum esse labore expedita ipsum sed explicabo dignissimos animi hic. Perspiciatis voluptas iste illo! Sapiente quidem numquam expedita, maiores modi quaerat?</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit nostrum esse labore expedita ipsum sed explicabo dignissimos animi hic. Perspiciatis voluptas iste illo! Sapiente quidem numquam expedita, maiores modi quaerat?</p>
-        </>
+        <div className="wrapper">
+        <h1>Error 404</h1>
+        <h2>This page may have sailed away.</h2>
+        </div>
     )
 }
